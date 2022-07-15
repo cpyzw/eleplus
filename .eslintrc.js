8f18db17fffdb8482f235fa9ff68fd321ca5e526
yuzhiwen <946998295@qq.com>
2022-07-15 17:42:09 +0800

    fix: add eslint some lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: [require.resolve("zw-standard/eslint-config/vue"), "prettier", "plugin:prettier/recommended"],
+  extends: [require.resolve("zw-standard/eslint-config/vue"), "prettier", "plugin:vue/vue3-recommended"],
   rules: {
     "no-console": ["off"],
     "vue/v-on-style": ["off"],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 import ElementPlus from "element-plus";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
+import App from "./App.vue";
 import router from "./router/index";
 import pinia from "./store/index";
 
